7e398dea292e23ec2c3003feaf64075a75b6a394
tomizi <106354750+tomizi@users.noreply.github.com>
2023-01-25 11:01:16 +0100

    Update 1_Strona_główna.py

--- 1_Strona_główna.py
+++ 1_Strona_główna.py
@@ -5,7 +5,7 @@ import streamlit as st
 st.title('Strona główna')
 st.sidebar.success("Wybierz stronę")
 
-st.set_page_config(page_title='UMK w liczbach', page_icon = ':page_facing_up:',initial_sidebar_state='expanded',layout='wide')
+st.set_page_config(page_title='UMK w liczbach', page_icon = ':page_facing_up:',layout='wide')
 
 
 #st.sidebar.success("Wybierz stronę")
