9319bcb5ce031990b635ddf56b289329b1ecdb2a
tomizi <106354750+tomizi@users.noreply.github.com>
2023-01-26 12:34:45 +0100

    Update 2_Studenci.py

--- pages/2_Studenci.py
+++ pages/2_Studenci.py
@@ -112,7 +112,7 @@ st.markdown(
     """
 <style>
 [data-testid="stAppViewContainer"] > .main {background-image: url("https://login.umk.pl/themes/umk/images/logo-umk.png");
-background-size:contain;
+background-size:400px 400px;
 background-position: 1150px 100px;
 background-repeat: no-repeat;
 background-attachment: local;}
